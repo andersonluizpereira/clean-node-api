541eb2cdb563e69e004e2acc4850825a2fc5530b
Anderson Pereira <andersononus@hotmail.com>
2020-09-21 18:05:11 -0300

    0001 - Criar setup de projeto

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
+    preset: '@shelf/jest-mongodb'
+  }
+  
