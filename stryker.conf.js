2969c8cf6169c6e330fcdb5382c6f4b6e24fb0d4
Anderson Pereira <andy2903.alp@gmail.com>
2021-02-09 09:16:04 -0300

    chore: submit remove report

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,7 @@
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
 module.exports = {
-  mutate: ['src/domain/usecases/*.js'],
+  mutate: ['src/domain/usecases/*.js', '!src/domain/usecases/*.spec.js'],
   reporters: ['html', 'clear-text'],
   testRunner: 'jest',
   jest: {
